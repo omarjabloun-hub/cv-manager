d87cd76b3d942f2447ec03cd95a9648ff21419a0
Omar <omar.jabloun@insat.u-carthage.tn>
2023-05-07 18:06:29 +0100

    add GQL decorators in cv entity

--- src/cv/entities/cv.entity.ts
+++ src/cv/entities/cv.entity.ts
@@ -2,26 +2,36 @@ import { Skill } from '../../skill/entities/skill.entity';
 import { User } from '../../user/entities/user.entity';
 import {
   Column,
-  Entity, JoinTable,
+  Entity,
+  JoinTable,
   ManyToMany,
   ManyToOne,
-  PrimaryGeneratedColumn
-} from "typeorm";
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 @Entity()
+@ObjectType()
 export class Cv {
   @PrimaryGeneratedColumn()
+  @Field(() => Int, { description: 'id of the user' })
   id: number;
   @Column()
+  @Field(() => String, { description: 'name of the user' })
   name: string;
   @Column()
+  @Field(() => String, { description: 'firstname of the user' })
   firstname: string;
   @Column()
+  @Field(() => Int, { description: 'age of the user' })
   age: number;
+  @Field(() => String, { description: 'cin of the user' })
   @Column()
   cin: string;
   @Column()
+  @Field(() => String, { description: 'job of the user' })
   job: string;
   @Column()
+  @Field(() => String, { description: 'path of the user' })
   path: string;
   @ManyToMany(() => Skill, (skill) => skill.cvs)
   @JoinTable()
