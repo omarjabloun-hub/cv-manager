7cd12f49e77868880c08acd8e12b8de1c7ac7ee5
Omar <omar.jabloun@insat.u-carthage.tn>
2023-05-07 18:10:43 +0100

    add GQL decorators in skill and user entity

--- src/skill/entities/skill.entity.ts
+++ src/skill/entities/skill.entity.ts
@@ -1,11 +1,15 @@
 import { Cv } from '../../cv/entities/cv.entity';
-import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity,  ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 
 @Entity()
+@ObjectType()
 export class Skill {
   @PrimaryGeneratedColumn()
+  @Field(() => Int, { description: 'id of the user' })
   id: number;
   @Column()
+  @Field(() => String, { description: 'name of the user' })
   designation: string;
   @ManyToMany(() => Cv, (cv) => cv.skills)
   cvs: Cv[];
